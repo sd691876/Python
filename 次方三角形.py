cb011b7d14e2d71420c6452390dafa85f62942be
sd691876 <55582065+sd691876@users.noreply.github.com>
2020-04-28 23:51:57 +0800

    4.28

---
+++ 次方三角形.py
@@ -0,0 +1,13 @@
+def Power_Pyramid(integer):
+    for i in range(integer):
+        for j in range(integer-i,0,-1):
+            print("   ", end = "")
+        for k in range(-i,i+1):
+            if(k != 0 and k != 1):
+                print(format(abs(2)**(i-abs(k)),">3d"), end = "")
+        print("")
+            
+while(1):
+    integer = eval(input("Enter integer(1 to 15) to show the pyramid: "))
+    Power_Pyramid(integer)
+    Pyramid(integer)
